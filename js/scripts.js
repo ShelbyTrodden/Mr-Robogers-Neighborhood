2b876c34bf0a1796e16bdede4c538206d0168ce8
Shelby Trodden <shelby.trodden@gmail.com>
2022-02-13 10:31:44 -0600

    edit HTML and README text

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // UI Logic
 
-function noInputtedWord() {
+function noInputtedNumber() {
   for (let i=0; i < arguments.length; i++) {
     if (arguments[i].trim().length === 0) {
       return true;
@@ -11,33 +11,13 @@ function noInputtedWord() {
 
 // Business Logic
 
-function wordCounter(text) {
-  if (noInputtedWord(text)) {
+function RobogersNeighborhood(Num) {
+  if (noInputtedNumber(Num)) {
     return 0;
   }
-  let wordCount = 0;
-  const wordArray = text.split(" ");
-  wordArray.forEach(function(element) {
-    if (!Number(element)) {
-      wordCount++;
-    }
-  });
-  return wordCount;
+
 }
 
-function numberOfOccurrencesInText(word, text) {
-  if (noInputtedWord(word, text)) {
-    return 0;
-  }
-  const wordArray = text.split(" ");
-  let wordCount = 0;
-  wordArray.forEach(function(element) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++;
-    }
-  });
-  return wordCount;
-}
 
 // UI Logic
 
@@ -45,31 +25,4 @@ function boldPassage(word, text) {
   if (noInputtedWord(word, text)) {
     return "";
   }
-  let htmlString = "<p>";
-  let textArray = text.split(" ");
-  textArray.forEach(function(element, index) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      htmlString = htmlString.concat("<b>" + element + "</b>");
-    } else {
-      htmlString = htmlString.concat(element);
-    }
-    if (index !== (textArray.length - 1)) {
-      htmlString = htmlString.concat(" ");
-    }
-  });
-  return htmlString + "</p>";
 }
-
-$(document).ready(function(){
-  $("form#word-counter").submit(function(event){
-    event.preventDefault();
-    const passage = $("#text-passage").val();
-    const word = $("#word").val();
-    const wordCount = wordCounter(passage);
-    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
-    $("#total-count").html(wordCount);
-    $("#selected-count").html(occurrencesOfWord);
-    $("#bolded-passage").html(boldPassage(word, passage));
-  });
-});
- 
